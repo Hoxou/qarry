c664ff93b75a02134b796c816877e4c71e86a26a
Victor Toupar <victor.toupar@gmail.com>
2024-05-08 18:14:32 +0800

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,138 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Paving a Quality Future',
+
+
+  customFields: {
+    subheadline: 'Mine through our extensive repository of blog posts and educational materials, and unearth your potential in Quality Assurance.'
+  },
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Qarry',
+        logo: {
+          alt: 'Qarry Logo',
+          src: 'img/qarry_logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Learning Materials',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Resources',
+            items: [
+              {
+                label: 'Recent blog posts',
+                to: '/blog',
+              },
+              {
+                label: 'Materials',
+                to: '/docs',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Medium',
+                href: 'https://medium.com/@victor.toupar',
+              },
+              {
+                label: 'Instagram',
+                href: 'https://instagram.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'My LinkedIn',
+                href: 'https://linkedin.com/in/victor-toupar/',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Qarry, Paving a quality future.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+
+
+export default config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,65 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+import StonePebble from '@site/static/img/stone_pebble.svg';
+import StoneLearn from '@site/static/img/stone_learn.svg';
+import StoneFoundation from '@site/static/img/stone_foundation.svg';
+
+const FeatureList = [
+  {
+    title: 'Quick Start for Beginners',
+    imgSrc: '/img/stone_pebble.svg', // Assuming the SVGs are in the public/img folder
+    description: (
+      <>
+        Start your Quality Assurance journey with easy-to-follow tutorials and practical guides.
+      </>
+    ),
+  },
+  {
+    title: 'Dive Deep into QA Principles',
+    imgSrc: '/img/stone_learn.svg',
+    description: (
+      <>
+        Explore essential QA principles and techniques through in-depth articles and <code>learning materials</code>.
+      </>
+    ),
+  },
+  {
+    title: 'Build Quality into Your Future',
+    imgSrc: '/img/stone_foundation.svg',
+    description: (
+      <>
+        Learn how to integrate quality practices into your development process, ensuring a solid foundation for future projects.
+      </>
+    ),
+  },
+];
+
+function Feature({ imgSrc, title, description }) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <img src={imgSrc} alt={title} className={styles.featureSvg} />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
